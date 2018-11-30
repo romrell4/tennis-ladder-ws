493ba49c55cfa32842a7b0638384d1c494c7e17a
Eric Romrell <eric_romrell@byu.edu>
2018-11-30 13:05:11 -0700

    Finished da and added pymysql

--- src/da.py
+++ src/da.py
@@ -1,3 +1,67 @@
+from domain import *
+import pymysql
+import os
 
 class Dao:
-    pass
+    def __init__(self):
+        try:
+            self.conn = pymysql.connect(os.environ["DB_HOST"], user = (os.environ["DB_USERNAME"]), passwd = (os.environ["DB_PASSWORD"]), db = (os.environ["DB_DATABASE_NAME"]), autocommit = True)
+        except Exception as e:
+            print("ERROR: Could not connect to MySQL", e)
+            raise ServiceException("Failed to connect to database")
+
+    def get_ladders(self):
+        return self.get_list(Ladder, "SELECT * FROM ladders ORDER BY START_DATE DESC")
+
+    def get_players(self, ladder_id):
+        return self.get_list(Player, "select users.ID, ladders.ID, users.NAME, scores.SCORE FROM scores JOIN users ON scores.USER_ID = users.ID join ladders on scores.LADDER_ID = ladders.ID WHERE scores.LADDER_ID = %s ORDER BY scores.SCORE DESC", ladder_id)
+
+    def create_match(self, match):
+        match_id = self.insert("INSERT INTO matches (WINNER_ID, LOSER_ID, WINNER_SET1_SCORE, LOSER_SET1_SCORE, WINNER_SET2_SCORE, LOSER_SET2_SCORE, WINNER_SET3_SCORE, LOSER_SET3_SCORE) values (%s, %s, %s, %s, %s, %s, %s, %s)", *match.get_insert_properties())
+        return self.get_one(Match, "SELECT * FROM matches WHERE ID = %s", match_id)
+
+    def update_score(self, user_id, ladder_id, new_score):
+        self.execute("UPDATE scores SET SCORE = %s WHERE USER_ID = %s and LADDER_ID = %s", new_score, user_id, ladder_id)
+
+    ### UTILS ###
+
+    def get_list(self, klass, sql, *args):
+        try:
+            with self.conn.cursor() as cur:
+                cur.execute(sql, args)
+                results = []
+                for row in cur.fetchall():
+                    results.append(klass(*row))
+                return results
+        except Exception as e:
+            print(e)
+            raise ServiceException("Error getting data from database")
+
+    def get_one(self, klass, sql, *args):
+        try:
+            with self.conn.cursor() as cur:
+                cur.execute(sql, args)
+                row = cur.fetchone()
+                if row is not None:
+                    return klass(*row)
+                return None
+        except Exception as e:
+            print(e)
+            raise ServiceException("Error getting data from database")
+
+    def insert(self, sql, *args):
+        try:
+            with self.conn.cursor() as cur:
+                cur.execute(sql, args)
+                return cur.lastrowid
+        except Exception as e:
+            print(e)
+            raise ServiceException("Error inserting data into database")
+
+    def execute(self, sql, *args):
+        try:
+            with self.conn.cursor() as cur:
+                cur.execute(sql, args)
+        except Exception as e:
+            print(e)
+            raise ServiceException("Error executing database command")
--- src/domain.py
+++ src/domain.py
@@ -7,15 +7,23 @@ class Ladder:
         self.ladder_id, self.name, self.start_date, self.end_date = ladder_id, name, str(start_date), str(end_date)
 
 class Score:
-    def __init__(self, score_id, user_id, ladder_id, score):
-        self.score_id, self.user_id, self.ladder_id, self.score = score_id, user_id, ladder_id, score
+    def __init__(self, user_id, ladder_id, score):
+        self.user_id, self.ladder_id, self.score = user_id, ladder_id, score
 
 class Match:
     def __init__(self, match_id, winner_id, loser_id, winner_set1_score, loser_set1_score, winner_set2_score, loser_set2_score, winner_set3_score, loser_set3_score):
         self.match_id, self.winner_id, self.loser_id, self.winner_set1_score, self.loser_set1_score, self.winner_set2_score, self.loser_set2_score, self.winner_set3_score, self.loser_set3_score = match_id, winner_id, loser_id, winner_set1_score, loser_set1_score, winner_set2_score, loser_set2_score, winner_set3_score, loser_set3_score
+
+    def get_insert_properties(self):
+        return [self.winner_id, self.loser_id, self.winner_set1_score, self.loser_set1_score, self.winner_set2_score, self.loser_set2_score, self.winner_set3_score, self.loser_set3_score]
 
 #### Non-DB Objects ####
 
 class Player:
     def __init__(self, user_id, ladder_id, name, score):
         self.user_id, self.ladder_id, self.name, self.score = user_id, ladder_id, name, score
+
+class ServiceException(Exception):
+    def __init__(self, message, status_code = 500):
+        self.error_message = message
+        self.status_code = status_code
