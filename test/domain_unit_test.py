878345669520d411509b64ecc2413a0758f8b6b4
Eric Romrell <eric_romrell@byu.edu>
2018-12-01 16:28:34 -0700

    Added domain tests and some domain changes

---
+++ test/domain_unit_test.py
@@ -0,0 +1,48 @@
+import unittest
+
+from domain import Match, DomainException
+
+class Test(unittest.TestCase):
+    def test_validate_match(self):
+        def assert_error(expected_error_message, match):
+            with self.assertRaises(DomainException) as e:
+                match.validate()
+            self.assertEqual(DomainException.MESSAGE_FORMAT.format(expected_error_message), e.error_message)
+
+        # Test match with null winner id
+        assert_error("Missing winner_id", Match(None, None, 1, 6, 0, 6, 0))
+
+        # Test match with null loser id
+        assert_error("Missing loser_id", Match(None, 1, None, 6, 0, 6, 0))
+
+    def test_is_valid_set(self):
+        self.assertFalse(Match.is_valid_set(0, 0))
+        self.assertFalse(Match.is_valid_set(2, 1))
+        self.assertFalse(Match.is_valid_set(10, 0))
+        self.assertFalse(Match.is_valid_set(6, 6))
+        self.assertFalse(Match.is_valid_set(5, 6))
+        self.assertFalse(Match.is_valid_set(7, 4))
+        self.assertFalse(Match.is_valid_set(7, 7))
+        self.assertFalse(Match.is_valid_set(6, -1))
+        self.assertTrue(Match.is_valid_set(7, 6))
+        self.assertTrue(Match.is_valid_set(6, 7))
+        self.assertTrue(Match.is_valid_set(5, 7))
+        self.assertTrue(Match.is_valid_set(7, 5))
+        self.assertTrue(Match.is_valid_set(6, 4))
+        self.assertTrue(Match.is_valid_set(4, 6))
+        self.assertTrue(Match.is_valid_set(6, 0))
+        self.assertTrue(Match.is_valid_set(0, 6))
+
+    def test_is_valid_tiebreak(self):
+        self.assertFalse(Match.is_valid_tiebreak(0, 0))
+        self.assertFalse(Match.is_valid_tiebreak(6, 1))
+        self.assertFalse(Match.is_valid_tiebreak(7, 9))
+        self.assertFalse(Match.is_valid_tiebreak(10, 10))
+        self.assertFalse(Match.is_valid_tiebreak(10, 9))
+        self.assertFalse(Match.is_valid_tiebreak(8, 11))
+        self.assertFalse(Match.is_valid_tiebreak(10, -1))
+        self.assertTrue(Match.is_valid_tiebreak(10, 0))
+        self.assertTrue(Match.is_valid_tiebreak(0, 10))
+        self.assertTrue(Match.is_valid_tiebreak(10, 8))
+        self.assertTrue(Match.is_valid_tiebreak(11, 9))
+        self.assertTrue(Match.is_valid_tiebreak(150, 148))
