d6e274df833b03f97e6679b214dff2d442efd69b
Eric Romrell <eric_romrell@byu.edu>
2019-01-08 10:28:45 -0700

    Added manager tests and function

--- src/bl.py
+++ src/bl.py
@@ -1,5 +1,6 @@
-from domain import User, ServiceException, Match
 from datetime import datetime
+
+from domain import User, ServiceException, Match
 
 class Manager:
     INVALID_RANKING_DISTANCE = 12
@@ -25,6 +26,29 @@ class Manager:
 
     def get_players(self, ladder_id):
         # The database handles all the sorting and derived fields
+        return self.dao.get_players(ladder_id)
+
+    def add_player_to_ladder(self, ladder_id, code):
+        if self.user is None:
+            raise ServiceException("Unable to authenticate", 401)
+        elif ladder_id is None:
+            raise ServiceException("Null ladder_id param", 400)
+
+        # Look up ladder
+        if self.dao.get_ladder(ladder_id) is None:
+            raise ServiceException("No ladder with id: '{}'".format(ladder_id), 404)
+
+        # Look up ladder code
+        real_code = self.dao.get_ladder_code(ladder_id)
+
+        # If sure that if a code exists, that it matches
+        if real_code is not None and real_code != code:
+            raise ServiceException("Invalid code", 400)
+
+        # Create the new player, tying the user to the ladder
+        self.dao.create_player(self.user.user_id, ladder_id)
+
+        # Return the new list of players in that ladder (which should include the new player)
         return self.dao.get_players(ladder_id)
 
     def get_matches(self, ladder_id, user_id):
