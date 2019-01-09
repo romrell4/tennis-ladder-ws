b13915437b381ac4f0a4d37c7f33d189f03ce270
Eric Romrell <eric_romrell@byu.edu>
2019-01-08 17:55:39 -0700

    Explicit selects

--- src/da.py
+++ src/da.py
@@ -4,7 +4,7 @@ import os
 
 class Dao:
     PLAYER_SQL_PREFIX = """
-        select u.*, l.ID as LADDER_ID, p.SCORE,
+        select u.ID, u.NAME, u.EMAIL, u.PHONE_NUMBER, u.PHOTO_URL, l.ID as LADDER_ID, p.SCORE,
           (select count(*) + 1 from players where LADDER_ID = %s and SCORE > p.SCORE) as RANKING,
           (select count(*) as WINS from matches where LADDER_ID = %s and WINNER_ID = u.ID) as WINS,
           (select count(*) as WINS from matches where LADDER_ID = %s and LOSER_ID = u.ID) as LOSSES
@@ -28,16 +28,16 @@ class Dao:
             raise ServiceException("Failed to connect to database")
 
     def get_user(self, user_id):
-        return self.get_one(User, "SELECT * FROM users where ID = %s", user_id)
+        return self.get_one(User, "select ID, NAME, EMAIL, PHONE_NUMBER, PHOTO_URL from users where ID = %s", user_id)
 
     def create_user(self, user):
-        self.insert("INSERT INTO users (ID, NAME, EMAIL, PHONE_NUMBER, PHOTO_URL) values (%s, %s, %s, %s, %s)", *user.get_insert_properties())
+        self.insert("insert into users (ID, NAME, EMAIL, PHONE_NUMBER, PHOTO_URL) values (%s, %s, %s, %s, %s)", *user.get_insert_properties())
 
     def get_ladders(self):
-        return self.get_list(Ladder, "SELECT * FROM ladders ORDER BY START_DATE DESC")
+        return self.get_list(Ladder, "select ID, NAME, START_DATE, END_DATE, DISTANCE_PENALTY_ON from ladders order by START_DATE DESC")
 
     def get_ladder(self, ladder_id):
-        return self.get_one(Ladder, "select * from ladders where ID = %s", ladder_id)
+        return self.get_one(Ladder, "select ID, NAME, START_DATE, END_DATE, DISTANCE_PENALTY_ON from ladders where ID = %s", ladder_id)
 
     def get_players(self, ladder_id):
         return self.get_list(Player, self.PLAYERS_SQL, ladder_id, ladder_id, ladder_id, ladder_id)
@@ -46,20 +46,20 @@ class Dao:
         return self.get_one(Player, self.PLAYER_SQL, ladder_id, ladder_id, ladder_id, ladder_id, user_id)
 
     def create_player(self, ladder_id, user_id):
-        self.execute("INSERT INTO players (USER_ID, LADDER_ID, SCORE) VALUES (%s, %s, 0)", user_id, ladder_id)
+        self.execute("insert into players (USER_ID, LADDER_ID, SCORE) values (%s, %s, 0)", user_id, ladder_id)
 
     def update_score(self, user_id, ladder_id, new_score_to_add):
-        self.execute("UPDATE players SET SCORE = SCORE + %s WHERE USER_ID = %s and LADDER_ID = %s", new_score_to_add, user_id, ladder_id)
+        self.execute("UPDATE players set SCORE = SCORE + %s where USER_ID = %s and LADDER_ID = %s", new_score_to_add, user_id, ladder_id)
 
     def get_matches(self, ladder_id, user_id):
-        return self.get_list(Match, "SELECT * FROM matches where LADDER_ID = %s and (WINNER_ID = %s or LOSER_ID = %s)", ladder_id, user_id, user_id)
+        return self.get_list(Match, "select ID, LADDER_ID, MATCH_DATE, WINNER_ID, LOSER_ID, WINNER_SET1_SCORE, LOSER_SET1_SCORE, WINNER_SET2_SCORE, LOSER_SET2_SCORE, WINNER_SET3_SCORE, LOSER_SET3_SCORE from matches where LADDER_ID = %s and (WINNER_ID = %s or LOSER_ID = %s)", ladder_id, user_id, user_id)
 
     def create_match(self, match):
-        match_id = self.insert("INSERT INTO matches (LADDER_ID, MATCH_DATE, WINNER_ID, LOSER_ID, WINNER_SET1_SCORE, LOSER_SET1_SCORE, WINNER_SET2_SCORE, LOSER_SET2_SCORE, WINNER_SET3_SCORE, LOSER_SET3_SCORE) values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", *match.get_insert_properties())
-        return self.get_one(Match, "SELECT * FROM matches WHERE ID = %s", match_id)
+        match_id = self.insert("insert into matches (LADDER_ID, MATCH_DATE, WINNER_ID, LOSER_ID, WINNER_set1_SCORE, LOSER_set1_SCORE, WINNER_set2_SCORE, LOSER_set2_SCORE, WINNER_set3_SCORE, LOSER_set3_SCORE) values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", *match.get_insert_properties())
+        return self.get_one(Match, "select ID, LADDER_ID, MATCH_DATE, WINNER_ID, LOSER_ID, WINNER_SET1_SCORE, LOSER_SET1_SCORE, WINNER_SET2_SCORE, LOSER_SET2_SCORE, WINNER_SET3_SCORE, LOSER_SET3_SCORE from matches where ID = %s", match_id)
 
     def get_ladder_code(self, ladder_id):
-        return self.get_one(str, "select code from ladder_codes where LADDER_ID = %s", ladder_id)
+        return self.get_one(str, "select CODE from ladder_codes where LADDER_ID = %s", ladder_id)
 
     ### UTILS ###
 
